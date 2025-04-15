cc8f4e4fe19f4a6918fb0562e55964285bd65520
Stanislav Olshanskyi <stasolsh77@gmail.com>
2025-04-15 20:45:20 +0300

    migrate-to-gradle (#5)
    * Add small fixes
    
    * migrate to gradle
    
    ---------
    
    Co-authored-by: Stanislav Olshanskyi <stasolsh77@gmail.ccom>

--- docker-local/run.bat
+++ docker-local/run.bat
@@ -7,7 +7,7 @@ set ROOT=%cd%
 docker compose -f docker-compose.yml stop
 
 cd %ROOT%/nba-statistics
-call mvn -T 1C clean install -DskipTests=true
+call ./gradlew clean build -DskipTests=true
 
 cd %ROOT%/
 docker compose -f docker-compose.yml up --build -d --no-deps
